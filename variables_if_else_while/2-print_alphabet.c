e8a6406e3efbd6f7644bd2f8511607dcd3b7a3cb
ZiyaSenior <safaraliyevziya@gmail.com>
2025-12-20 20:37:03 +0000

    tasks

---
+++ variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+
+/**
+ * main - Prints alphabet
+ *
+ * Return: 0 if successful
+ */
+int man(void)
+{
+	char a;
+
+	for (a = 'a'; a <= 'z' ; a++)
+		putchar(a);
+	putchar('\n');
+	return (0);
+}
+	
