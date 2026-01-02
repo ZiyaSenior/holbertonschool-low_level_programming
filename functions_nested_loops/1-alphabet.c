2da902e8b8bdac36695a32a91bb8f378d33e0e2f
ZiyaSenior <safaraliyevziya@gmail.com>
2026-01-02 16:10:44 +0000

    tasks

---
+++ functions_nested_loops/1-alphabet.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * print_alphabet - entry point 
+ * Return: 0 if successful
+ */
+void print_alphabet(void)
+{
+	char letter;
+
+	for (letter = 'a'; letter <= 'z'; letter++)
+		_putchar('\n');
+}
