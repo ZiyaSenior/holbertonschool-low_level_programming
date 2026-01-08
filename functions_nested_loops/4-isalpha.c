25c35225d0664fc26ba3fe9a9c92a349ef29cc31
ZiyaSenior <safaraliyevziya@gmail.com>
2026-01-08 06:30:07 +0000

    tasks

--- functions_nested_loops/4-isalpha.c
+++ functions_nested_loops/4-isalpha.c
@@ -8,9 +8,10 @@
  */
 int _isalpha(int c)
 {
-    if ((c >= 'A' && c <= 'Z') || (c >= 'a' && c <= 'z'))
-    {
-        return (1);
-    }
-    return (0);
+	if ((c >= 'A' && c <= 'Z') || (c >= 'a' && c <= 'z'))
+	{
+		return (1);
+	}
+	return (0);
 }
+
