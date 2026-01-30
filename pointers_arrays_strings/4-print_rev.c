22147d7b413b9f8bd6a440a5fab44bd2a7469a22
ZiyaSenior <safaraliyevziya@gmail.com>
2026-01-30 08:38:43 +0000

    tasks

---
+++ pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * print_rev - prints a string in reverse
+ * @s: pointer to the string
+ */
+void print_rev(char *s)
+{
+	int len;
+	int i;
+
+	len = 0;
+
+	while (s[len] != '\0')
+	{
+		len++;
+	}
+
+	for (i = len - 1; i >= 0; i--)
+	{
+		_putchar(s[i]);
+	}
+
+	_putchar('\n');
+}
