a3f6e38d44f55f3f50de9706bbf325b155932890
ZiyaSenior <safaraliyevziya@gmail.com>
2026-01-30 10:30:11 +0000

    tasks

---
+++ pointers_arrays_strings/100-atoi.c
@@ -0,0 +1,33 @@
+#include "main.h"
+
+/**
+ * _atoi - converts a string to an integer
+ * @s: pointer to the string
+ *
+ * Return: converted integer
+ */
+int _atoi(char *s)
+{
+	int i = 0, sign = 1, res = 0;
+
+	/* keç bütün boşluqları və işarələri */
+	while (s[i] != '\0')
+	{
+		if (s[i] == '-')
+			sign *= -1;
+		else if (s[i] == '+')
+			;
+		else if (s[i] >= '0' && s[i] <= '9')
+			break;
+		i++;
+	}
+
+	/* qur integer */
+	while (s[i] >= '0' && s[i] <= '9')
+	{
+		res = res * 10 + (s[i] - '0');
+		i++;
+	}
+
+	return (res * sign);
+}
