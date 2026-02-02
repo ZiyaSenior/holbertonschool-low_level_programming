cab86ed9a7363661c06711489211c8ce3b4f43be
ZiyaSenior <safaraliyevziya@gmail.com>
2026-02-02 12:16:03 +0000

    tasks

---
+++ pointers_arrays_strings/7-leet.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+/**
+ * leet - encodes a string into 1337
+ * @str: the string to encode
+ *
+ * Return: pointer to the modified string
+ */
+char *leet(char *str)
+{
+	int i, j;
+	char letters[] = "aAeEoOtTlL";
+	char leet_nums[] = "4433007711";
+
+	for (i = 0; str[i] != '\0'; i++)
+	{
+		for (j = 0; letters[j] != '\0'; j++)
+		{
+			if (str[i] == letters[j])
+			{
+				str[i] = leet_nums[j];
+				break;
+			}
+		}
+	}
+
+	return (str);
+}
