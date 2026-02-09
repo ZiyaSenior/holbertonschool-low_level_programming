73545cc533d2081620c3a2aa7584a62e3ccc5a3c
ZiyaSenior <safaraliyevziya@gmail.com>
2026-02-09 07:47:46 +0000

    tasks

--- pointers_arrays_strings/0-memset.c
+++ pointers_arrays_strings/0-memset.c
@@ -1,21 +1,23 @@
 #include "main.h"
 
 /**
- * _memset - memory sahəsini sabit baytla doldurur
- * @s: doldurulacaq yaddaşın başlanğıc ünvanı
- * @b: yazılacaq bayt
- * @n: neçə bayt doldurulacaq
+ * _memset - fills memory with a constant byte
+ * @s: pointer to the memory area
+ * @b: constant byte to fill with
+ * @n: number of bytes to fill
  *
- * Return: s ünvanı
+ * Return: pointer to the memory area s
  */
 char *_memset(char *s, char b, unsigned int n)
 {
-    unsigned int i;
+	unsigned int i;
 
-    for (i = 0; i < n; i++)
-    {
-        s[i] = b;
-    }
+	i = 0;
+	while (i < n)
+	{
+		s[i] = b;
+		i++;
+	}
 
-    return (s);
+	return (s);
 }
