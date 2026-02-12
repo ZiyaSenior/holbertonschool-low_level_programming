47d5e7a5e8abcf57d1d6493a7b937c432da20137
ZiyaSenior <safaraliyevziya@gmail.com>
2026-02-12 12:26:35 +0000

    tasks

--- argc_argv/0-whatsmyname.c
+++ argc_argv/0-whatsmyname.c
@@ -2,14 +2,14 @@
 
 /**
  * main - prints the name of the program
- * @argc: argument count (unused)
- * @argv: argument vector (array of strings)
+ * @argc: number of arguments
+ * @argv: array of arguments
  *
  * Return: 0 on success
  */
 int main(int argc, char *argv[])
 {
-    (void)argc;  /* argc is unused */
+    (void)argc; /* argc is unused */
     printf("%s\n", argv[0]);
-    return 0;
+    return (0);
 }
