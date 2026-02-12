c909e0d216efddedee9c0f94342546f8d3fe56af
ZiyaSenior <safaraliyevziya@gmail.com>
2026-02-12 12:30:18 +0000

    tasks

--- argc_argv/0-whatsmyname.c
+++ argc_argv/0-whatsmyname.c
@@ -9,7 +9,8 @@
  */
 int main(int argc, char *argv[])
 {
-    (void)argc; /* argc is unused */
-    printf("%s\n", argv[0]);
-    return (0);
+	(void)argc; /* argc is unused */
+	printf("%s\n", argv[0]);
+	return (0);
 }
+
