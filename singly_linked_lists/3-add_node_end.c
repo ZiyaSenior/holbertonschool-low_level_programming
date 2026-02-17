5670e97fb96d96f70fcb9ec0de39c2c568c84402
ZiyaSenior <safaraliyevziya@gmail.com>
2026-02-17 13:10:52 +0000

    tasks

--- singly_linked_lists/3-add_node_end.c
+++ singly_linked_lists/3-add_node_end.c
@@ -1,52 +1,41 @@
-#include <stdlib.h>
 #include "lists.h"
+#include <stdlib.h>
+#include <string.h>
 
 /**
- * add_node_end - adds a new node at the end of a list_t list
- * @head: pointer to pointer to head of the list
- * @str: string to add
- *
- * Return: address of new node, or NULL if it failed
+ * add_node_end - adds node to the end of the list
+ * @head: head of node
+ * @str: element must be added
+ * Return: pointer to new list if successful
  */
+
 list_t *add_node_end(list_t **head, const char *str)
 {
-	list_t *new_node, *last;
-	unsigned int len;
+	list_t *new, *tmp;
+	int counter = 0;
 
-	if (!str)
+	new = malloc(sizeof(list_t));
+	if (new == NULL)
 		return (NULL);
-
-	new_node = malloc(sizeof(list_t));
-	if (!new_node)
-		return (NULL);
-
-	new_node->str = strdup(str);
-	if (!new_node->str)
+	while (str[counter] != '\0')
+		counter++;
+	new->str = strdup(str);
+	if (new->str == NULL)
 	{
-		free(new_node);
+		free(new);
 		return (NULL);
 	}
+	new->len = counter;
+	new->next = NULL;
 
-	/* strlen əvəzinə loop ilə uzunluğu hesabla */
-	len = 0;
-	while (new_node->str[len] != '\0')
-		len++;
-	new_node->len = len;
-
-	new_node->next = NULL;
-
-	if (!*head) /* list boşdursa */
+	if (*head == NULL)
+		*head = new;
+	else
 	{
-		*head = new_node;
-		return (new_node);
+		tmp = *head;
+		while (tmp->next != NULL)
+			tmp = tmp->next;
+		tmp->next = new;
 	}
-
-	/* Son node-u tap və onun next-inə bağla */
-	last = *head;
-	while (last->next)
-		last = last->next;
-
-	last->next = new_node;
-
-	return (new_node);
+	return (new);
 }
