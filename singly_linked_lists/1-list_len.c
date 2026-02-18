2e3d4821f5ca276182eddb03ae9e37e8927856e8
ZiyaSenior <safaraliyevziya@gmail.com>
2026-02-18 10:45:03 +0000

    tasks

--- singly_linked_lists/1-list_len.c
+++ singly_linked_lists/1-list_len.c
@@ -4,7 +4,7 @@
  * list_len - returns the number of elements in a linked list
  * @h: pointer to the head of the list
  *
- * Return: number of nodes
+ * Return: number of elements
  */
 size_t list_len(const list_t *h)
 {
