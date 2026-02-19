0b74a62c320a0a82d5728d9de58d627efb8c49f4
ZiyaSenior <safaraliyevziya@gmail.com>
2026-02-19 16:59:22 +0000

    tasks

---
+++ file_io/1-create_file.c
@@ -0,0 +1,39 @@
+#include "main.h"
+#include <fcntl.h>
+#include <unistd.h>
+
+/**
+ * create_file - creates a file and writes text to it
+ * @filename: name of the file to create
+ * @text_content: NULL terminated string to write
+ *
+ * Return: 1 on success, -1 on failure
+ */
+
+int create_file(const char *filename, char *text_content)
+{
+	int fd, len = 0, i, written;
+
+	if (filename == NULL)
+		return (-1);
+
+	fd = open(filename, O_CREAT | O_WRONLY | O_TRUNC, 0600);
+	if (fd == -1)
+		return (-1);
+
+	if (text_content != NULL)
+	{
+		for (i = 0; text_content[i]; i++)
+			len++;
+
+		written = write(fd, text_content, len);
+		if (written == -1)
+		{
+			close(fd);
+			return (-1);
+		}
+	}
+
+	close(fd);
+	return (1);
+}
