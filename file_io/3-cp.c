48b67faa1c0ac50089e63372efff8d3499802303
ZiyaSenior <safaraliyevziya@gmail.com>
2026-02-19 17:07:51 +0000

    tasks

--- file_io/3-cp.c
+++ file_io/3-cp.c
@@ -1,75 +1,71 @@
-#include <fcntl.h>
-#include <unistd.h>
-#include <stdlib.h>
-#include <stdio.h>
-
-#define BUFFER_SIZE 1024
+#include "main.h"
+#define BUFRSIZE 1204
+#define ER STDERR_FILENO
 
 /**
- * main - copies content from one file to another
- * @argc: number of arguments
- * @argv: array of arguments
- *
- * Return: 0 on success
- */
+* copy_file - copies the content of a file to another file
+* @file1: file to copy from
+* @fil2: file to copy to
+* Return: nothing, or exit on failure
+**/
 
-int main(int argc, char *argv[])
+void copy_file(const char *file1, char *fil2)
 {
-	int fd_from, fd_to, r, w;
-	char buffer[BUFFER_SIZE];
+	ssize_t count, count_out;
+	int f1, f2, fcls1, fcls2;
+	char bufr[BUFRSIZE];
+	mode_t mode;
 
-	if (argc != 3)
+	f1 = open(file1, O_RDONLY);
+	if (f1 == -1)
 	{
-		dprintf(2, "Usage: cp file_from file_to\n");
-		exit(97);
-	}
-
-	fd_from = open(argv[1], O_RDONLY);
-	if (fd_from == -1)
-	{
-		dprintf(2, "Error: Can't read from file %s\n", argv[1]);
+		dprintf(ER, "Error: Can't read from file %s\n", file1);
 		exit(98);
 	}
-
-	fd_to = open(argv[2], O_CREAT | O_WRONLY | O_TRUNC, 0664);
-	if (fd_to == -1)
+	mode = S_IRUSR | S_IWUSR | S_IRGRP | S_IWGRP | S_IROTH;
+	f2 = open(fil2, O_CREAT | O_WRONLY | O_TRUNC, mode);
+	if (f2 == -1)
+		dprintf(ER, "Error: Can't write to %s\n", fil2), exit(99);
+	count = 1;
+	while (count > 0)
 	{
-		dprintf(2, "Error: Can't write to %s\n", argv[2]);
-		close(fd_from);
-		exit(99);
-	}
-
-	while ((r = read(fd_from, buffer, BUFFER_SIZE)) > 0)
-	{
-		w = write(fd_to, buffer, r);
-		if (w == -1)
+		count = read(f1, bufr, BUFRSIZE);
+		if (count == -1)
 		{
-			dprintf(2, "Error: Can't write to %s\n", argv[2]);
-			close(fd_from);
-			close(fd_to);
-			exit(99);
+			dprintf(ER, "Error: Can't read from file %s\n", file1);
+			exit(98);
+		}
+		if (count > 0)
+		{
+			count_out = write(f2, bufr, count);
+			if (count_out == -1)
+			{
+				dprintf(ER, "Error: Can't write to %s\n", fil2);
+				exit(99);
+			}
 		}
 	}
+	fcls1 = close(f1);
+	if (fcls1 == -1)
+		dprintf(ER, "Error: Can't close fd %d\n", fcls1), exit(100);
+	fcls2 = close(f2);
+	if (fcls2 == -1)
+		dprintf(ER, "Error: Can't close fd %d\n", fcls2), exit(100);
+}
 
-	if (r == -1)
+/**
+* main - copies the content of a file to another file
+* @argc: number of arguements
+* @argv: pointer to arguement
+* Return: 0 on success, or exit with error message
+**/
+int main(int argc, char **argv)
+{
+	if (argc != 3)
 	{
-		dprintf(2, "Error: Can't read from file %s\n", argv[1]);
-		close(fd_from);
-		close(fd_to);
-		exit(98);
+		dprintf(ER, "Usage: cp file_from file_to\n");
+		exit(97);
 	}
-
-	if (close(fd_from) == -1)
-	{
-		dprintf(2, "Error: Can't close fd %d\n", fd_from);
-		exit(100);
-	}
-
-	if (close(fd_to) == -1)
-	{
-		dprintf(2, "Error: Can't close fd %d\n", fd_to);
-		exit(100);
-	}
-
+	copy_file(argv[1], argv[2]);
 	return (0);
 }
