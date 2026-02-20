0e1236700d82b0a4db7a94e21a0a13ca7bd7d920
ZiyaSenior <safaraliyevziya@gmail.com>
2026-02-20 11:35:16 +0000

    tasks

--- hash_tables/3-hash_table_set.c
+++ hash_tables/3-hash_table_set.c
@@ -1,60 +1,55 @@
 #include "hash_tables.h"
-#include <stdlib.h>
 #include <string.h>
+#include <stdlib.h>
 
 /**
- * hash_table_set - adds an element to the hash table
- * @ht: pointer to the hash table
- * @key: key string
- * @value: value associated with the key
+ * hash_table_set - Adds or updates an element in a hash table.
+ * @ht: The hash table.
+ * @key: The key (must not be an empty string).
+ * @value: The value associated with the key (must be duplicated).
  *
- * Return: 1 if success, 0 otherwise
+ * Return: 1 if succeeded, 0 otherwise.
  */
 int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 {
-	hash_node_t *new_node;
-	hash_node_t *current;
-	unsigned long int index;
+	hash_node_t *new_node, *current;
 	char *value_dup;
+	unsigned long int index;
 
-	if (ht == NULL || key == NULL || *key == '\0' || value == NULL)
+	if (!ht || !key || !*key || !value)
 		return (0);
-
 	index = key_index((const unsigned char *)key, ht->size);
 	current = ht->array[index];
-
 	while (current)
 	{
 		if (strcmp(current->key, key) == 0)
 		{
-			value_dup = strdup(value);
-			if (value_dup == NULL)
-				return (0);
-
 			free(current->value);
-			current->value = value_dup;
+			current->value = strdup(value);
+			if (!current->value)
+				return (0);
 			return (1);
 		}
 		current = current->next;
 	}
-
-	new_node = malloc(sizeof(hash_node_t));
-	if (new_node == NULL)
+	value_dup = strdup(value);
+	if (!value_dup)
 		return (0);
-
-	new_node->key = strdup(key);
-	new_node->value = strdup(value);
-
-	if (new_node->key == NULL || new_node->value == NULL)
+	new_node = malloc(sizeof(hash_node_t));
+	if (!new_node)
 	{
-		free(new_node->key);
-		free(new_node->value);
-		free(new_node);
+		free(value_dup);
 		return (0);
 	}
-
+	new_node->key = strdup(key);
+	if (!new_node->key)
+	{
+		free(new_node);
+		free(value_dup);
+		return (0);
+	}
+	new_node->value = value_dup;
 	new_node->next = ht->array[index];
 	ht->array[index] = new_node;
-
 	return (1);
 }
