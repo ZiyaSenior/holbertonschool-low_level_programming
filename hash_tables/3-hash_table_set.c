5a019eb9df8b88ea9c72b35ae231fa07ac45993b
ZiyaSenior <safaraliyevziya@gmail.com>
2026-02-20 11:31:00 +0000

    tasks

---
+++ hash_tables/3-hash_table_set.c
@@ -0,0 +1,61 @@
+#include "hash_tables.h"
+#include <stdlib.h>
+#include <string.h>
+
+/**
+ * hash_table_set - adds an element to the hash table
+ * @ht: hash table
+ * @key: key string
+ * @value: value associated with key
+ *
+ * Return: 1 if success, 0 if failed
+ */
+
+int hash_table_set(hash_table_t *ht, const char *key, const char *value)
+{
+	hash_node_t *new_node;
+	hash_node_t *temp;
+	unsigned long int index;
+	char *value_copy;
+
+	if (ht == NULL || key == NULL || *key == '\0')
+		return (0);
+
+	index = key_index((const unsigned char *)key, ht->size);
+	temp = ht->array[index];
+
+	while (temp)
+	{
+		if (strcmp(temp->key, key) == 0)
+		{
+			value_copy = strdup(value);
+			if (value_copy == NULL)
+				return (0);
+
+			free(temp->value);
+			temp->value = value_copy;
+			return (1);
+		}
+		temp = temp->next;
+	}
+
+	new_node = malloc(sizeof(hash_node_t));
+	if (new_node == NULL)
+		return (0);
+
+	new_node->key = strdup(key);
+	new_node->value = strdup(value);
+
+	if (new_node->key == NULL || new_node->value == NULL)
+	{
+		free(new_node->key);
+		free(new_node->value);
+		free(new_node);
+		return (0);
+	}
+
+	new_node->next = ht->array[index];
+	ht->array[index] = new_node;
+
+	return (1);
+}
